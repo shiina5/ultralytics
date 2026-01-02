edba53127b1ba8c70f51b0a0ba95faa05c1bc6e3
shiina5 <3276475310@qq.com>
2026-01-02 14:19:19 +0800

    Refactor IoU loss calculation in loss.py
    Updated IoU calculation to use EIoU and Focal parameters, handling tuple return for loss_iou.
    
    Signed-off-by: shiina5 <3276475310@qq.com>

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -125,8 +125,12 @@ class BboxLoss(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor]:
         """Compute IoU and DFL losses for bounding boxes."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
-        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
-        loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+       iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, EIoU=True, Focal=True) 
+if type(iou) is tuple:
+    loss_iou = ((1.0 - iou[0]) * iou[1].detach() * weight).sum() / target_scores_sum
+else:
+    loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+
 
         # DFL loss
         if self.dfl_loss:
